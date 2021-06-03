554d5c9b7869ffb7343d1f9a6eb52b41215b1f1a
beark93 <beark93@gmail.com>
2021-06-03 23:42:43 +0900

    day 1 - 소환사명으로 계정정보 불러오기 / 계정ID로 최근 경기 데이터 불러오기 기능 추가

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\SummonerController;
+use App\Http\Controllers\MatchController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/summoner/{username}', [SummonerController::class, 'getSummonerByUsername']);
+
+Route::get('/matches/{accountID}', [MatchController::class, 'getMacthesByAccountID']);
